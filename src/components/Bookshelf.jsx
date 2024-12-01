c9fccad133d1d9474ce2174f2a492c1d913a1805
deijahmonay <deijah@shack15.com>
2024-12-01 12:20:02 -0800

    create and set up bookshelf.jsx

---
+++ src/components/Bookshelf.jsx
@@ -0,0 +1,7 @@
+<div className="bookshelfDiv">
+  <div className="formDiv">
+    <h3>Add a Book</h3>
+    {/* Form will go here */}
+  </div>
+  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+</div>
