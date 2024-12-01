c4a194a24896b99e466f1ca0c266d3403d4dd0d6
deijahmonay <deijah@shack15.com>
2024-12-01 12:25:23 -0800

    set up bookshelf function + export

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,7 +1,14 @@
-<div className="bookshelfDiv">
-  <div className="formDiv">
-    <h3>Add a Book</h3>
+const Bookshelf = () => {
+  return (
+    <div className="bookshelfDiv">
+      <div className="formDiv">
+        <h3>Add a Book</h3>
     {/* Form will go here */}
-  </div>
-  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-</div>
+      </div>
+      <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+    </div>
+)
+}
+
+
+export default Bookshelf;
